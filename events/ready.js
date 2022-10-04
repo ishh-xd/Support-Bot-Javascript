4a729fc54b22f6f748311aa332983a246183dfbe
Ishh <88816438+AdityaOP2008@users.noreply.github.com>
2022-10-04 19:50:10 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,7 +5,10 @@ name:'ready',
 run: async(client)=>{
 
 let x=client.guilds.cache.get(client.config.serverId)
-
+    // Ishh On Top
+ // Coded by Ishh
+// https://github.com/AdityaOP2008/Support-Bot-Javascript
+    
 if(!x){
 console.error('Bot is not present in the guild.')
 console.error('Bot is not present in the guild.')
@@ -22,3 +25,7 @@ console.log("🟢 "+client.user.tag)
 
 }
 }
+    // Ishh On Top
+ // Coded by Ishh
+// https://github.com/AdityaOP2008/Support-Bot-Javascript
+    
