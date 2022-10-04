2eecf4cdb234bac2d77e48a9241f9f7bb753bc9b
Ishh <88816438+AdityaOP2008@users.noreply.github.com>
2022-10-04 19:36:36 +0530

    env.js
    Config file.

---
+++ env.js
@@ -0,0 +1,9 @@
+module.exports ={
+token:"" || process.env.TOKEN, // Token
+embedColor:"#303037",// Embed color
+owner:[""], // Developer id
+serverId:"", // Server id
+helpDeskChannel:"", // Help-desk channel id
+supportChannelId:"", // Support channel id
+ghostPing: true, // Enabled true or false?
+}
